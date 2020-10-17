eebb5d9532cc304a8c1ee53d013c3c2c3b6366d0
b0kch01 <nath49ers@gmail.com>
2020-10-16 18:47:23 -0700

    new sections

---
+++ discordbot.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const { Message } = require("discord.js");
+const { prefix, token } = require("./bot/bot-config.json");
+
+bot.on("ready", () => {
+    console.log("[✔]: Poggers! We are running!");
+});
+
+bot.on("message", msg => {
+    if (msg.content === "hello") msg.channel.send(`👋 Hello, ${msg.author}`);
+    if (msg.content === "are developers better than designers?") msg.channel.send(`yes.`);
+    if (msg.content === "are designers better than developers?") msg.channel.send(`no.`);
+
+    if (!msg.content.startsWith(prefix)) return;
+    if (msg.author.bot) return;
+
+    const args = msg.content.substring(prefix.length).split(" ");
+
+    switch (args[0]) {
+        case "start":
+            msg.channel.send("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n@here - 🏁 Game started!\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+                .then(message => {
+                    startGame(message)
+                });
+            break;
+    }
+});
+
+// PC LIST THINGS
+
+
+// EMOJI THINGS
+
+function getEmoji(message, name) {
+    return message.guild.emojis.cache.find(emoji => emoji.name == name)
+}
+
+async function startGame(message) {
+    try {
+        await message.react("🔈");
+        await message.react("🔊");
+        await message.react("🏁");
+    } catch (error) {
+        console.error('One of the emojis failed to react.' + error);
+    }
+}
+
+bot.login(token);
+
+module.exports = {
+    init: bot.login(token)
+}
