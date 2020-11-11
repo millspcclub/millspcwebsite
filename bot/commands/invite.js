d6ee78c9e2b53cd7007d5d827ffa0c80659b3aa5
b0kch01 <nath49ers@gmail.com>
2020-11-10 21:54:26 -0800

    Added new command

--- bot/commands/invite.js
+++ bot/commands/invite.js
@@ -1,9 +1,20 @@
 function invite(msg, args) {
     if (msg.member.voice) {
-        msg.channel.send(`[${args}] Come join my voice channel!\n<@${msg.member.voice.channel.id}>`);
+        replyWithInvite(msg, args.join(""));
     }
 }
 
+async function replyWithInvite(message, args) {
+    let invite = await message.channel.createInvite({
+            maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
+            maxUses: 1 // maximum times it can be used
+        },
+        `Requested with command by ${message.author.tag}`
+    ).catch(console.log);
+    
+    message.reply(invite ? `[${args}]Here's your invite: ${invite}` : "There has been an error during the creation of the invite.");
+}
+    
 module.exports = {
     name: 'invite',
     description: 'Invite people into your voice channel!',
